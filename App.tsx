a8feef524d860b5be18682402b6814db32bd77e6
tariq-shuvo <tariq10043@gmail.com>
2022-03-30 16:07:50 +0600

    redux has been added

--- App.tsx
+++ App.tsx
@@ -15,23 +15,29 @@ import UserRegisterScreen from './src/screens/UserRegisterScreen';
 
 const Stack = createStackNavigator()
 
+// Redux Initialization
+import {Provider} from 'react-redux'
+import store from './src/store'
+
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start App'>
-          <Stack.Screen name='Start App' component={StartScreen} options={{
-            headerShown: false
-          }}/>
-          <Stack.Screen name='Login' component={UserLoginScreen} />
-          <Stack.Screen name='User List' component={UserListScreen} />
-          <Stack.Screen name='User Create' component={UserRegisterScreen} />
-          <Stack.Screen name='Student Create' component={AddStudentScreen} />
-          <Stack.Screen name='Student List' component={StudentListScreen} />
-          <Stack.Screen name='Student Update' component={UpdateStudentScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Start App'>
+            <Stack.Screen name='Start App' component={StartScreen} options={{
+              headerShown: false
+            }}/>
+            <Stack.Screen name='Login' component={UserLoginScreen} />
+            <Stack.Screen name='User List' component={UserListScreen} />
+            <Stack.Screen name='User Create' component={UserRegisterScreen} />
+            <Stack.Screen name='Student Create' component={AddStudentScreen} />
+            <Stack.Screen name='Student List' component={StudentListScreen} />
+            <Stack.Screen name='Student Update' component={UpdateStudentScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
   );
 }
 
